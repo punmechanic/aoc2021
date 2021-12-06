c078fb264d441f443a5956218a027650894d3591
Dan <dan@aredherring.tech>
2021-12-06 17:08:45 +0000

    add solution for day 1

--- day1/main.go
+++ day1/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
 
+func main() {
+	r := bufio.NewReader(os.Stdin)
+
+	first := true
+	var acc uint
+	var prev uint16
+	for {
+		line, _, err := r.ReadLine()
+		if errors.Is(err, io.EOF) {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		raw, err := strconv.ParseUint(string(line), 10, 16)
+		if err != nil {
+			log.Fatalf("failed to parse %q: %s", line, err)
+		}
+
+		current := uint16(raw)
+		if current > prev && !first {
+			acc++
+		}
+
+		prev = current
+		first = false
+	}
+
+	println(acc)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trinitroglycerin/aoc2021
+
+go 1.17
