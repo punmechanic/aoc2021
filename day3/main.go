2d6fe78660d7451402f6c19407b6e02877656594
Dan <dan@aredherring.tech>
2021-12-06 21:01:06 +0000

    convert application to use bitset structure

--- day3/main.go
+++ day3/main.go
@@ -10,16 +10,9 @@ import (
 	"github.com/trinitroglycerin/aoc2021/pkg/bitset"
 )
 
-const (
-	ONE uint8 = 49
-)
-
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	var lines uint
-	// Rather than attempt to calculate the epsilon and gamma at the same time, we can count the number of 1s we read for each bit.
-	// If the number of 1s is larger than at least half the number of lines, then we know that 1 is the most common bit, and 0 the least.
-	var oneCounts []uint
+	var bitSets []bitset.BitSet
 	for {
 		line, _, err := r.ReadLine()
 		if errors.Is(err, io.EOF) {
@@ -28,31 +21,9 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if lines == 0 {
-			oneCounts = make([]uint, len(line))
-		}
-
-		for idx, char := range line {
-			if char == ONE {
-				oneCounts[idx]++
-			}
-		}
-
-		lines++
+		bitSets = append(bitSets, bitset.FromString(string(line)))
 	}
 
-	half := lines / 2
-	gammaBits := bitset.NewBitSet(len(oneCounts))
-	epsilonBits := bitset.NewBitSet(len(oneCounts))
-	for idx, count := range oneCounts {
-		if count > half {
-			gammaBits.Set(idx)
-		} else {
-			epsilonBits.Set(idx)
-		}
-	}
-
-	gamma := gammaBits.Uint()
-	epsilon := epsilonBits.Uint()
-	println(gamma * epsilon)
+	gammaBits := bitset.MostCommon(bitSets)
+	println(gammaBits.Uint() * gammaBits.Negate().Uint())
 }
--- pkg/bitset/bitset.go
+++ pkg/bitset/bitset.go
@@ -1,5 +1,46 @@
 package bitset
 
+const one rune = '1'
+
+// MostCommon returns a BitSet that represents the most common bits in the given slice of BitSets.
+// For example, a slice containing BitSets 1001, 1100, 1011 and 0001 would return the BitSet 1001.
+// The size of the ultimate BitSet is based on the size of the first in the slice; each bitset should be aligned.
+// The behaviour of a slice of unaligned bitsets is undefined.
+func MostCommon(sets []BitSet) BitSet {
+	bitLen := sets[0].BitLength()
+	oneCounts := make([]int, len(sets))
+	for _, set := range sets {
+		for i := 0; i < bitLen; i++ {
+			if set[i] {
+				oneCounts[i]++
+			}
+		}
+	}
+
+	bs := NewBitSet(bitLen)
+	half := len(sets) / 2
+	for idx, count := range oneCounts {
+		if count > half {
+			bs.Set(idx)
+		}
+	}
+
+	return bs
+}
+
+// FromString reads a BitSet from a string by converting each character to a number - 1s and 0s are the only correct input.
+// Any other character will be ignored.
+func FromString(str string) BitSet {
+	bs := NewBitSet(len(str))
+	for idx, char := range str {
+		if char == one {
+			bs.Set(idx)
+		}
+	}
+
+	return bs
+}
+
 type BitSet []bool
 
 func NewBitSet(bits int) BitSet {
@@ -7,7 +48,7 @@ func NewBitSet(bits int) BitSet {
 }
 
 func (b BitSet) BitLength() int {
-	return len([]bool(b)) - 1
+	return len([]bool(b))
 }
 
 func (b BitSet) Set(idx int) {
@@ -26,9 +67,18 @@ func (b BitSet) Unset(idx int) {
 	b[idx] = false
 }
 
+func (b BitSet) Negate() BitSet {
+	bs := NewBitSet(b.BitLength())
+	for idx, bit := range b {
+		bs[idx] = !bit
+	}
+
+	return bs
+}
+
 func (b BitSet) Uint() uint {
 	var o uint
-	shift := b.BitLength()
+	shift := b.BitLength() - 1
 	for _, bit := range b {
 		var b uint8
 		if bit == true {
