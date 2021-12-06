282068b9d55fdbb624f06609a74998f73d10529d
Dan <dan@aredherring.tech>
2021-12-06 17:15:57 +0000

    add solution for day 2

--- day2/main.go
+++ day2/main.go
@@ -6,18 +6,39 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
 
+	var depth, hpos int64
 	for {
-		_, _, err := r.ReadLine()
+		line, _, err := r.ReadLine()
 		if errors.Is(err, io.EOF) {
 			break
 		} else if err != nil {
 			log.Fatal(err)
 		}
 
+		parts := strings.Split(string(line), " ")
+		v, err := strconv.ParseInt(parts[1], 10, 32)
+		if err != nil {
+			log.Fatalf("failed to parse %q: %s", parts[1], err)
+		}
+
+		switch parts[0] {
+		case "forward":
+			hpos += v
+		case "down":
+			depth += v
+		case "up":
+			depth -= v
+		default:
+			log.Fatalf("unrecognised direction %q\n", parts[0])
+		}
 	}
+
+	println(depth * hpos)
 }
